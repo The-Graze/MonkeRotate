c28dd20f687c215a3aa442c74095bdf1fae95b66
A Haunted Army <extreme15@hotmail.co.uk>
2021-05-24 06:04:51 +0100

    added project for editor files, started making editor classes. editor project recurisvely searches and adds all cs files in the Confg Types folder

---
+++ Swim/Config Types/SwimConfig.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace MonkeSwim.Config
+{
+    [System.Serializable]
+    public class MonkeSwimConfig : MonoBehaviour
+    {
+        [Tooltip("this overrides GlobalSettings and any custom settinsg inside a zone.\n" +
+                 "defaults: MaxSpeed 6.5f, Acceleration 1.1f, Resistence 0f, GravityAmount 0f")]
+        public static bool UseDefault = false;
+
+        [Tooltip("if this is true the mod will be enabled accross the entire map")]
+        public static bool EntireMap = false;
+
+        [Tooltip("these settings will apply to every zone unless overriden by the trigger")]
+        public static MonkeSwimSettings GlobalSettings;
+
+        //settings that will be used if UseDefault is true
+        public static readonly MonkeSwimSettings DefaultSettings = new MonkeSwimSettings{ MaxSpeed = 6.5f, Acceleration = 1.1f, Resistence = 0f, GravityAmount = 0f };
+    }
+
+    [System.Serializable]
+    public struct MonkeSwimSettings
+    {
+        public float MaxSpeed;
+        public float Acceleration;
+        public float Resistence;
+        public float GravityAmount;
+    }
+}
---
+++ Swim/Config Types/SwimTriggers.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace MonkeSwim.Config
+{
+    class SwimTrigger : MonoBehaviour
+    {
+        public bool OverrideGlobal = false;
+        public MonkeSwimSettings ZoneSettings;
+
+    #if GAME
+        public void OnTriggerEnter(Collider collider)
+        {
+            if (!collider.gameObject.name.Equals("Body Collider")) return;
+
+            Debug.Log(collider.gameObject.name + " has entered trigger " + gameObject.name);
+            Debug.Log("setting stats of " + transform.parent.localPosition);
+
+            /* old stuff
+            Swim.SetStats(transform.parent.localPosition);
+            Swim.EnableMod(true);
+            */
+        }
+        public void OnTriggerExit(Collider collider)
+        {
+            if (!collider.gameObject.name.Equals("Body Collider")) return;
+
+            Debug.Log(collider.gameObject.name + " has left trigger " + gameObject.name);
+            //Swim.EnableMod(false);
+        }
+    #endif
+    }
+}
