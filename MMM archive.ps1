f2e97e5f22e50fa9dbc6e1eb6773132617d0151a
AHauntedArmy <extreme15@hotmail.co.uk>
2021-11-14 04:43:30 +0000

    updated readme, added powershell script

---
+++ MMM archive.ps1
@@ -0,0 +1,14 @@
+# Needs to be at least that version, or mmm can't read the archive
+#Requires -Modules @{ ModuleName="Microsoft.PowerShell.Archive"; ModuleVersion="1.2.3" }
+$Name = "MonkeSwim" # Replace with your mods name
+$Version = "v1.0.0"
+
+mkdir BepInEx\plugins\$Name
+mkdir BepInEx\plugins\MonkeMapLoader\CustomMaps
+
+cp Swim\bin\Release\netstandard2.0\$Name.dll BepInEx\plugins\$Name\
+cp GravityTechnicalTest.gtmap BepInEx\Plugins\MonkeMapLoader\CustomMaps\
+
+Compress-Archive -force .\BepInEx\ $Name-$Version.zip
+
+rmdir .\BepInEx\ -Recurse
